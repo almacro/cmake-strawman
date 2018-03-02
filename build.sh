df3ccaa9859374dd8eef6a08191651e80c4d565e
Al Thompson <alm4x1mu5@gmail.com>
2018-03-01 17:35:45 -0800

    Adds a simple build script

--- build.sh
+++ build.sh
@@ -1,22 +1,38 @@
 #!/usr/bin/env bash
 
-if [ $# -ge 1 ]
-then
-  if [ $1="clean" ]
-  then
-    if [ -e ./build/Makefile ]
-    then
-      cd ./build/; make clean; cd - 1>/dev/null
-    fi
-    rm -rf build
-  else
-    echo "Command not found"
-    exit 1
-  fi
-else
+_do_build() {
   if [ ! -d ./build/ ]
   then
     mkdir ./build/
   fi
   cd ./build/; cmake -G "Unix Makefiles" ..; make; cd - 1>build.log
-fi
+
+}
+
+_do_test() {
+  _do_build
+  cd ./build/; make test; cd - 1>build.log
+}
+
+_do_clean() {
+  if [ -e ./build/Makefile ]
+  then
+    cd ./build/; make clean; cd - 1>/dev/null
+  fi
+  rm -rf build
+}
+
+case "$1" in
+  "build")          
+    _do_build
+    ;;
+  "test")
+    _do_test
+    ;;
+  "clean")
+    _do_clean
+    ;;
+  *)
+    _do_build
+    ;;
+esac
